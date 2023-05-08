47dd29428723583fef912c5f458996c8f6b23279
areef44 <areef.empat.empat@gmail.com>
2023-05-08 13:04:16 +0700

    initial commit

---
+++ panic_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPanicHandler(t *testing.T) {
+
+	router := httprouter.New()
+
+	router.PanicHandler = func(w http.ResponseWriter, r *http.Request, e interface{}) {
+		fmt.Fprint(w, "panic : ", e)
+	}
+
+	router.GET("/", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		panic("Ups")
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:3000/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "panic : Ups", string(body))
+}
